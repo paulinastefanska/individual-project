360e5040544020f0bd1c5cb6de1a4235f6cce72c
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-05-10 19:16:28 +0200

    Add active class to current page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Add active class to the current menu link to highlight it
+var menuLeft = document.getElementById("leftMenu");
+var menuPages = menuLeft.getElementsByClassName("menu_link");
+for (var i = 0; i < menuPages.length; i++) {
+  menuPages[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
