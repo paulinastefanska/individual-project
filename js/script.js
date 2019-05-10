57df327c5456c4348b12cfac4cc94b96183de311
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-05-10 22:04:43 +0200

    Add pages toggle

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 // Add active class to the current page to highlight it
 var menuLeft = document.getElementById("leftMenu");
 var menuPages = menuLeft.getElementsByClassName("menu_link");
+
 for (var i = 0; i < menuPages.length; i++) {
   menuPages[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
@@ -10,3 +11,29 @@ for (var i = 0; i < menuPages.length; i++) {
   this.className += " active";
   });
 }
+
+// Pages toggling
+for (var i = 0; i < menuPages.length; i++) {
+  menuPages[i].addEventListener("click", function () {
+    var menuPages = this.getAttribute("href");
+    var content = document.querySelector(menuPages + '_section');
+    var all = document.querySelectorAll(".section");
+
+    if (content) {
+
+    for (var j = 0; j < all.length; j++) {
+    	all[j].classList.add("hidden");
+      	}
+      	content.classList.remove("hidden");
+    	}
+  });
+}
+
+// Menu show
+function toggleMenu(visible) {
+  document.querySelector('.menu').classList.toggle('show', visible)
+}
+document.querySelector('.menu_hamburger').addEventListener('click', function(e) {
+  e.preventDefault();
+  toggleMenu()
+});
