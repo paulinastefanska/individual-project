74c52fa038346adca1e9e2f0237c9ed8f9c2a22f
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-05-12 16:35:04 +0200

    Improve hamburger add short menu

--- js/script.js
+++ js/script.js
@@ -28,52 +28,22 @@ for (var i = 0; i < menuPages.length; i++) {
     	}
   });
 }
-/*
-// Menu show
+
+// Menu hide - short menu show
 function toggleMenu(visible) {
-  document.querySelector('.menu').classList.toggle('show', visible)
+	document.querySelector('.menu').classList.toggle('hide', visible)
+  	document.querySelector('.short').classList.toggle('show', visible)
 }
-document.querySelector('.menu_hamburger').addEventListener('click', function(e) {
-  e.preventDefault();
-  toggleMenu()
-});*/
+document.querySelector('.hamburger').addEventListener('click', function(e) {
+	e.preventDefault();
+	toggleMenu()
+});
 
-var theToggle = document.getElementById('toggle');
-
-// hasClass
-function hasClass(elem, className) {
-	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+function toggleShort(visible) {
+	document.querySelector('.menu').classList.remove('hide')
+  	document.querySelector('.short').classList.remove('show')
 }
-// addClass
-function addClass(elem, className) {
-    if (!hasClass(elem, className)) {
-    	elem.className += ' ' + className;
-    }
-}
-// removeClass
-function removeClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
-	if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    }
-}
-// toggleClass
-function toggleClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0 ) {
-            newClass = newClass.replace( " " + className + " " , " " );
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    } else {
-        elem.className += ' ' + className;
-    }
-}
-
-theToggle.onclick = function() {
-   toggleClass(this, 'on');
-   return false;
-}
+document.querySelector('.short_hamburger').addEventListener('click', function(e) {
+	e.preventDefault();
+	toggleShort()
+});
