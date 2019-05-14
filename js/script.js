f2fdee4138a4228b88ad624be9fdb07f039fe6da
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-05-14 12:01:51 +0200

    Improve menu toggle and inputs

--- js/script.js
+++ js/script.js
@@ -29,23 +29,33 @@ for (var i = 0; i < menuPages.length; i++) {
   });
 }
 
-// Short menu show
-function toggleMenu(visible) {
-	document.querySelector('.menu').classList.toggle('hide', visible)
-  	document.querySelector('.short').classList.toggle('show', visible)
+// Short menu show and fullwidth
+function toggleMenu(cur = document.querySelector('#navigation').className) {
+  if (cur == 'long')
+  {
+    document.querySelector('#navigation').classList.remove("long");
+    document.querySelector('#navigation').classList.add("short");
+    document.querySelector('#fullscreen').classList.remove("main");
+    document.querySelector('#fullscreen').classList.add("fullwidth");
+  }
+  else {
+    document.querySelector('#navigation').classList.remove("short");
+    document.querySelector('#navigation').classList.add("long");
+    document.querySelector('#fullscreen').classList.remove("fullwidth");
+    document.querySelector('#fullscreen').classList.add("main");
+  }
 }
 
 document.querySelector('#toggle').addEventListener('click', function(e) {
 	e.preventDefault();
- if (document.querySelector('#navigation').className == 'long')
- {
-   document.querySelector('#navigation').classList.remove("long");
-   document.querySelector('#navigation').classList.add("short");
- }
- else {
-   document.querySelector('#navigation').classList.remove("short");
-   document.querySelector('#navigation').classList.add("long");
- }	
+  toggleMenu();  
 });
 
+// Postback range slide
+document.querySelector('#triangle').style.marginLeft='61.9%';
+document.querySelector('#torange').style.width='61.9%';
+/*document.querySelector('#triangle').addEventListener('pointerdown', function(e){
+  var ct=document.querySelector('#triangle').style.marginLeft;   
+});*/
+    
 
